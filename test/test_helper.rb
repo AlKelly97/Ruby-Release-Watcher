65467e5e93dec0979812c650c675238609b913d0
AlKelly97 <x00138464@gmail.com>
2026-03-22 17:13:26 +0000

    adding db setup to `test_helper`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,15 @@ require "rack/test"
 
 require_relative "../app"
 
+require_relative "../db/setup"
+
 module TestHelpers
     def app
         ReleaseWatcher
+    end
+
+    def setup
+        DB[:projects].delete
     end
 end
 
